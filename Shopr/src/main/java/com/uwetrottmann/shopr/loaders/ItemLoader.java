ef0681abff7487a6ea0014784bb0a809e6225120
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-05-22 14:41:25 +0200

    Add item loader, for now only loading samples.

---
+++ Shopr/src/main/java/com/uwetrottmann/shopr/loaders/ItemLoader.java
@@ -0,0 +1,32 @@
+
+package com.uwetrottmann.shopr.loaders;
+
+import android.content.Context;
+
+import com.uwetrottmann.shopr.model.Item;
+import com.uwetrottmann.shopr.utils.Lists;
+
+import java.util.List;
+
+/**
+ * Returns a list of items based on the current user model.
+ */
+public class ItemLoader extends GenericSimpleLoader<List<Item>> {
+
+    public ItemLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    public List<Item> loadInBackground() {
+        List<Item> items = Lists.newArrayList();
+
+        for (int i = 0; i < 10; i++) {
+            Item item = new Item().id(i).name("Sample Item " + i).picture("sample.jpg").shopId(42);
+            items.add(item);
+        }
+
+        return items;
+    }
+
+}
