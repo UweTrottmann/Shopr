78e915a9c7dd8242c45a1bf60397755b2ec26b6d
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-05-22 14:40:46 +0200

    Add getters and setters for Item.

--- Shopr/src/main/java/com/uwetrottmann/shopr/model/Item.java
+++ Shopr/src/main/java/com/uwetrottmann/shopr/model/Item.java
@@ -1,10 +1,47 @@
+
 package com.uwetrottmann.shopr.model;
 
 public class Item {
 
-    public int id;
-    public String name;
-    public String picture;
-    public int shop_id;
+    private int id;
+    private String name;
+    private String picture;
+    private int shop_id;
+
+    public int id() {
+        return id;
+    }
+
+    public Item id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Item name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String picture() {
+        return picture;
+    }
+
+    public Item picture(String picture) {
+        this.picture = picture;
+        return this;
+    }
+
+    public int shopId() {
+        return shop_id;
+    }
+
+    public Item shopId(int shop_id) {
+        this.shop_id = shop_id;
+        return this;
+    }
 
 }
