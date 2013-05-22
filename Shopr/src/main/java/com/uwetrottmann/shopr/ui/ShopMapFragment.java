7a5563740f68f6451d6ad274d10d90b5561d33a8
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-05-22 13:56:10 +0200

    Add markers on long-pressing the map.

--- Shopr/src/main/java/com/uwetrottmann/shopr/ui/ShopMapFragment.java
+++ Shopr/src/main/java/com/uwetrottmann/shopr/ui/ShopMapFragment.java
@@ -2,8 +2,14 @@
 package com.uwetrottmann.shopr.ui;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class ShopMapFragment extends SupportMapFragment {
 
@@ -17,5 +23,14 @@ public class ShopMapFragment extends SupportMapFragment {
 
         // enable my location feature
         getMap().setMyLocationEnabled(true);
+        getMap().setOnMapLongClickListener(new OnMapLongClickListener() {
+
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                // Add marker at tap position
+                getMap().addMarker(new MarkerOptions().position(latLng).title(latLng.toString()));
+            }
+        });
+
     }
 }
