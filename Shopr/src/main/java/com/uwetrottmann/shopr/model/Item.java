1b1b9c8afe1cfcc87854f442f39dd5ee0a268fce
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-05-22 14:03:53 +0200

    Add first draft for item and shop model.

---
+++ Shopr/src/main/java/com/uwetrottmann/shopr/model/Item.java
@@ -0,0 +1,10 @@
+package com.uwetrottmann.shopr.model;
+
+public class Item {
+
+    public int id;
+    public String name;
+    public String picture;
+    public int shop_id;
+
+}
---
+++ Shopr/src/main/java/com/uwetrottmann/shopr/model/Shop.java
@@ -0,0 +1,11 @@
+package com.uwetrottmann.shopr.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Shop {
+
+    public int id;
+    public String name;
+    public LatLng location;
+
+}
