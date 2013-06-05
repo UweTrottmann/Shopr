91775b3e2afc26f7388ab66f0259020765e7b568
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-05 15:33:23 +0200

    Add first dummy test.

---
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/test/SimilarityTest.java
@@ -0,0 +1,14 @@
+package com.uwetrottmann.shopr.algorithm.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimilarityTest {
+
+    @Test
+    public void testSimilarity() {
+        fail("Not yet implemented");
+    }
+
+}
