e4ddf6835d9e3819b7ab4135cdf614e11c68b18e
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-05 18:39:24 +0200

    Write test with expected values.

--- Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/test/SimilarityTest.java
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/test/SimilarityTest.java
@@ -1,6 +1,13 @@
+
 package com.uwetrottmann.shopr.algorithm.test;
 
-import static org.junit.Assert.*;
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.fest.assertions.api.Assertions.failBecauseExceptionWasNotThrown;
+
+import com.uwetrottmann.shopr.algorithm.Similarity;
+import com.uwetrottmann.shopr.algorithm.model.Attributes;
+import com.uwetrottmann.shopr.algorithm.model.ClothingType;
+import com.uwetrottmann.shopr.algorithm.model.Color;
 
 import org.junit.Test;
 
@@ -8,7 +15,30 @@ public class SimilarityTest {
 
     @Test
     public void testSimilarity() {
-        fail("Not yet implemented");
+        Attributes a1 = new Attributes();
+        a1.color = new Color(Color.Value.RED);
+        a1.type = new ClothingType(ClothingType.Value.DRESS);
+
+        Attributes a2 = new Attributes();
+        a2.color = new Color(Color.Value.BLACK);
+        a2.type = null;
+
+        // different color
+        assertThat(Similarity.similarity(a1, a2)).isEqualTo(0.0);
+
+        // same color
+        a1.color = new Color(Color.Value.BLACK);
+        assertThat(Similarity.similarity(a1, a2)).isEqualTo(1.0);
+
+        // throw on no comparable attributes
+        a1.color = null;
+        try {
+            Similarity.similarity(a1, a2);
+
+            failBecauseExceptionWasNotThrown(IllegalArgumentException.class);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
 }
