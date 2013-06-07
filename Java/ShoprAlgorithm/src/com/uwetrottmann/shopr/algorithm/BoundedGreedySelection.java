a39650b70d086d8c6056d0e28d0f1299b013f778
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-07 10:13:56 +0200

    Implement sorting by quality.

--- Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/BoundedGreedySelection.java
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/BoundedGreedySelection.java
@@ -4,9 +4,27 @@ package com.uwetrottmann.shopr.algorithm;
 import com.uwetrottmann.shopr.algorithm.model.Item;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class BoundedGreedySelection {
+
+    private static final class QualityComparator implements Comparator<Item> {
+        @Override
+        public int compare(Item o1, Item o2) {
+            // is o2 smaller?
+            if (o1.quality() > o2.quality()) {
+                return -1;
+            }
+            // is o2 bigger?
+            if (o1.quality() < o2.quality()) {
+                return 1;
+            }
+            // they are equal!
+            return 0;
+        }
+    }
 
     public static final double ALPHA = 0.5;
 
@@ -39,11 +57,14 @@ public class BoundedGreedySelection {
      * quality first.
      */
     private static void sortByQuality(List<Item> caseBase, List<Item> recommendations, Query query) {
-        // TODO Sort by quality
+        // Calculate current quality
         for (Item item : caseBase) {
-            double quality = ALPHA * item.querySimilarity()
-                    + (1 - ALPHA) * relativeDiversity(item, recommendations);
+            item.quality(ALPHA * item.querySimilarity()
+                    + (1 - ALPHA) * relativeDiversity(item, recommendations));
         }
+
+        // sort by highest quality first
+        Collections.sort(caseBase, new QualityComparator());
     }
 
     /**
