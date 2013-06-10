e5fb3044489d3e645ff5f5dda410f1746f02e7fa
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-10 10:08:46 +0200

    Add getters and setters to Critique.

--- Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/Critique.java
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/Critique.java
@@ -1,10 +1,29 @@
+
 package com.uwetrottmann.shopr.algorithm;
 
 import com.uwetrottmann.shopr.algorithm.model.Item;
 
 public class Critique {
 
-    public Item item;
-    public Feedback feedback;
+    private Item item;
+    private Feedback feedback;
+
+    public Item item() {
+        return item;
+    }
+
+    public Critique item(Item item) {
+        this.item = item;
+        return this;
+    }
+
+    public Feedback feedback() {
+        return feedback;
+    }
+
+    public Critique feedback(Feedback feedback) {
+        this.feedback = feedback;
+        return this;
+    }
 
 }
