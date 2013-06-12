ae3f215e5814c1ffac8efd2fb2a9909ac065329a
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-12 10:43:32 +0200

    Add critique activity.

---
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/ui/CritiqueActivity.java
@@ -0,0 +1,126 @@
+
+package com.uwetrottmann.shopr.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.uwetrottmann.shopr.R;
+import com.uwetrottmann.shopr.model.Item;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class CritiqueActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_critique);
+        // Show the Up button in the action bar.
+        setupActionBar();
+    }
+
+    /**
+     * Set up the {@link android.app.ActionBar}.
+     */
+    private void setupActionBar() {
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.critique, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class ItemFeatureAdapter extends BaseAdapter {
+
+        private static final int LAYOUT = R.layout.feature_row;
+        private Item mItem;
+        private Context mContext;
+        private LayoutInflater mLayoutInflater;
+
+        public ItemFeatureAdapter(Context context, Item item) {
+            mContext = context;
+            mItem = item;
+            mLayoutInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItem;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+
+            if (convertView == null) {
+                convertView = mLayoutInflater.inflate(LAYOUT, parent, false);
+
+                holder = new ViewHolder();
+                holder.title = (CheckBox) convertView.findViewById(R.id.checkBoxFeatureTitle);
+                holder.value = (TextView) convertView.findViewById(R.id.textViewFeatureValue);
+
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Item item = (Item) getItem(position);
+            holder.title.setText("Price");
+            holder.value.setText(NumberFormat.getCurrencyInstance(Locale.GERMANY).format(
+                    item.price().doubleValue()));
+
+            return convertView;
+        }
+
+    }
+
+    static class ViewHolder {
+        CheckBox title;
+        TextView value;
+    }
+
+}
