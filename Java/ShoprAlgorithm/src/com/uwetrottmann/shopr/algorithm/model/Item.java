3b85a2a795f3a62d0870bea326845c2642c392e7
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-13 13:50:14 +0200

    Add price and name to algorithm model of Item.

--- Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/model/Item.java
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/model/Item.java
@@ -1,12 +1,18 @@
 
 package com.uwetrottmann.shopr.algorithm.model;
 
+import java.math.BigDecimal;
+
 /**
  * Represents a item (e.g. clothing item), or one case in the case-base.
  */
 public class Item {
 
     private int id;
+
+    private String name;
+
+    private BigDecimal price;
 
     private Attributes attrs;
 
@@ -20,6 +26,24 @@ public class Item {
 
     public Item id(int id) {
         this.id = id;
+        return this;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Item name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public BigDecimal price() {
+        return price;
+    }
+
+    public Item price(BigDecimal price) {
+        this.price = price;
         return this;
     }
 
