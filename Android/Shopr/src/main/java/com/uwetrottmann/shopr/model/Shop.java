7fab48fa4a829cfc85e004d8f1aff96654fc254a
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-18 10:21:07 +0200

    Add getters and setters for Shop.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/model/Shop.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/model/Shop.java
@@ -1,11 +1,39 @@
+
 package com.uwetrottmann.shopr.model;
 
 import com.google.android.gms.maps.model.LatLng;
 
 public class Shop {
 
-    public int id;
-    public String name;
-    public LatLng location;
+    private int id;
+    private String name;
+    private LatLng location;
+
+    public int id() {
+        return id;
+    }
+
+    public Shop id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Shop name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public LatLng location() {
+        return location;
+    }
+
+    public Shop location(LatLng location) {
+        this.location = location;
+        return this;
+    }
 
 }
