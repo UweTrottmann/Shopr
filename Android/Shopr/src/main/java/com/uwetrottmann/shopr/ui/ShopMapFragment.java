420f763fd39849a2b810666878c03e4aa9eda397
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-18 11:23:08 +0200

    Center map on user location upon launch.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/ui/ShopMapFragment.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/ui/ShopMapFragment.java
@@ -1,9 +1,12 @@
 
 package com.uwetrottmann.shopr.ui;
 
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.uwetrottmann.shopr.model.Shop;
 import com.uwetrottmann.shopr.utils.ShopUtils;
@@ -12,6 +15,7 @@ import java.util.List;
 
 public class ShopMapFragment extends SupportMapFragment {
 
+    private static final int ZOOM_LEVEL_INITIAL = 14;
     public static final String TAG = "Shops Map";
 
     public static ShopMapFragment newInstance() {
@@ -19,18 +23,45 @@ public class ShopMapFragment extends SupportMapFragment {
     }
 
     private List<Shop> mShops;
+    private Location mLocation;
+    private boolean mIsInitialized;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        mIsInitialized = false;
 
         // enable my location feature
         getMap().setMyLocationEnabled(true);
 
         mShops = ShopUtils.getShopsSamples();
 
-        onDisplayShops();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        onInitializeMap();
+
+        onDisplayShops();
+    }
+
+    private void onInitializeMap() {
+        if (!mIsInitialized) {
+            mLocation = ((MainActivity) getActivity()).getLocation();
+            if (mLocation == null) {
+                return;
+            }
+
+            // move camera to current position
+            getMap().moveCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                            new LatLng(mLocation.getLatitude(), mLocation.getLongitude()), ZOOM_LEVEL_INITIAL));
+
+            mIsInitialized = true;
+        }
     }
 
     private void onDisplayShops() {
