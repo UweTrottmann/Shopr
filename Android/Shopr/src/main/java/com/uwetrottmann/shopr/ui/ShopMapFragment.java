2d9580fb5c481bd10777031a028b6d0176bc9605
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-18 11:33:17 +0200

    Draw circle around current user position.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/ui/ShopMapFragment.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/ui/ShopMapFragment.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.uwetrottmann.shopr.R;
 import com.uwetrottmann.shopr.model.Shop;
 import com.uwetrottmann.shopr.utils.ShopUtils;
 
@@ -15,7 +17,8 @@ import java.util.List;
 
 public class ShopMapFragment extends SupportMapFragment {
 
-    private static final int ZOOM_LEVEL_INITIAL = 14;
+    private static final int RADIUS_METERS = 2000;
+    private static final int ZOOM_LEVEL_INITIAL = 13;
     public static final String TAG = "Shops Map";
 
     public static ShopMapFragment newInstance() {
@@ -55,10 +58,17 @@ public class ShopMapFragment extends SupportMapFragment {
                 return;
             }
 
+            LatLng userPosition = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
             // move camera to current position
             getMap().moveCamera(
-                    CameraUpdateFactory.newLatLngZoom(
-                            new LatLng(mLocation.getLatitude(), mLocation.getLongitude()), ZOOM_LEVEL_INITIAL));
+                    CameraUpdateFactory.newLatLngZoom(userPosition, ZOOM_LEVEL_INITIAL));
+            // draw a circle around it
+            getMap().addCircle(new CircleOptions()
+                    .center(userPosition)
+                    .radius(RADIUS_METERS)
+                    .strokeColor(getResources().getColor(R.color.lilac))
+                    .strokeWidth(4)
+                    .fillColor(getResources().getColor(R.color.lilac_transparent)));
 
             mIsInitialized = true;
         }
