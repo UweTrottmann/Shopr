2a185ed5efd7d09b432111befeefe2e4ee4d1a8d
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-19 17:22:42 +0200

    Fix UtilsTest: use new put and get methods.

--- Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/test/UtilsTest.java
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/test/UtilsTest.java
@@ -19,44 +19,49 @@ public class UtilsTest {
     public void testSortBySimilarityToQuery() {
         // unsorted list of items
         ArrayList<Item> unsortedCases = new ArrayList<Item>();
+        unsortedCases
+                .
+                add(new Item().attributes(new Attributes().putAttribute(new Color(Color.Value.RED))));
         unsortedCases.
-                add(new Item().attributes(new Attributes().color(new Color(Color.Value.RED))));
-        unsortedCases.
-                add(new Item().attributes(new Attributes().color(new Color(Color.Value.BLACK))));
+                add(new Item().attributes(new Attributes()
+                        .putAttribute(new Color(Color.Value.BLACK))));
         unsortedCases
-                .add(new Item().attributes(new Attributes().color(new Color(Color.Value.RED))));
+                .add(new Item().attributes(new Attributes()
+                        .putAttribute(new Color(Color.Value.RED))));
         unsortedCases
-                .add(new Item().attributes(new Attributes().color(new Color(Color.Value.GREEN))));
+                .add(new Item().attributes(new Attributes().putAttribute(new Color(
+                        Color.Value.GREEN))));
         unsortedCases
-                .add(new Item().attributes(new Attributes().color(new Color(Color.Value.BLUE))));
+                .add(new Item().attributes(new Attributes()
+                        .putAttribute(new Color(Color.Value.BLUE))));
 
         // sample query
         Query query = new Query();
-        query.attributes().color(new Color(Color.Value.RED));
+        query.attributes().putAttribute(new Color(Color.Value.RED));
 
         Utils.sortBySimilarityToQuery(query, unsortedCases);
 
         // top two items are red?
         Utils.dumpToConsole(unsortedCases, query);
         assertThat(
-                unsortedCases.get(0).attributes().color().getValueWeights()[Color.Value.RED
+                unsortedCases.get(0).attributes().getAttributeById(Color.ID).getValueWeights()[Color.Value.RED
                         .ordinal()]).isEqualTo(1.0);
         assertThat(
-                unsortedCases.get(1).attributes().color().getValueWeights()[Color.Value.RED
+                unsortedCases.get(1).attributes().getAttributeById(Color.ID).getValueWeights()[Color.Value.RED
                         .ordinal()]).isEqualTo(1.0);
 
         // equal weight on all colors
-        query.attributes().color(new Color());
+        query.attributes().putAttribute(new Color());
 
         Utils.sortBySimilarityToQuery(query, unsortedCases);
 
         // nothing should have changed
         Utils.dumpToConsole(unsortedCases, query);
         assertThat(
-                unsortedCases.get(0).attributes().color().getValueWeights()[Color.Value.RED
+                unsortedCases.get(0).attributes().getAttributeById(Color.ID).getValueWeights()[Color.Value.RED
                         .ordinal()]).isEqualTo(1.0);
         assertThat(
-                unsortedCases.get(1).attributes().color().getValueWeights()[Color.Value.RED
+                unsortedCases.get(1).attributes().getAttributeById(Color.ID).getValueWeights()[Color.Value.RED
                         .ordinal()]).isEqualTo(1.0);
     }
 
