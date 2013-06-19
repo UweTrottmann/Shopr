356a7a56bd20a893b7e89dde136ea33a412acb8d
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-19 11:39:19 +0200

    Add Price attribute.

---
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/model/Price.java
@@ -0,0 +1,74 @@
+
+package com.uwetrottmann.shopr.algorithm.model;
+
+import com.uwetrottmann.shopr.algorithm.model.Attributes.AttributeValue;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+public class Price extends GenericAttribute {
+
+    public static final String ID = "price";
+
+    public enum Value implements AttributeValue {
+        SUB_25("less than 25 €"),
+        BETWEEN_25_50("25 to 50 €"),
+        BETWEEN_50_100("50 to 100 €"),
+        ABOVE_100("more than 100 €");
+
+        private String mDescriptor;
+
+        Value(String descriptor) {
+            mDescriptor = descriptor;
+        }
+
+        @Override
+        public String descriptor() {
+            return mDescriptor;
+        }
+
+        @Override
+        public int index() {
+            return ordinal();
+        }
+
+    }
+
+    public Price() {
+        int numValues = Value.values().length;
+        mValueWeights = new double[numValues];
+        Arrays.fill(mValueWeights, 1.0 / numValues);
+    }
+
+    public Price(BigDecimal price) {
+        mValueWeights = new double[Value.values().length];
+        Arrays.fill(mValueWeights, 0.0);
+
+        // determine price range
+        Value value;
+        if (price.doubleValue() < 25.0) {
+            value = Value.SUB_25;
+        } else if (price.doubleValue() < 50.0) {
+            value = Value.BETWEEN_25_50;
+        } else if (price.doubleValue() < 100.0) {
+            value = Value.BETWEEN_50_100;
+        } else {
+            value = Value.ABOVE_100;
+        }
+        
+        mValueWeights[value.ordinal()] = 1.0;
+        currentValue(value);
+    }
+
+    @Override
+    public String id() {
+        return ID;
+    }
+
+    @Override
+    public AttributeValue[] getValueSymbols() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
