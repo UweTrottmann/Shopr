b270654b2a838cb4bea85cfea9236e0124142eb1
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-24 11:19:35 +0200

    Add first URI matcher.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/provider/ShoprProvider.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/provider/ShoprProvider.java
@@ -2,15 +2,45 @@ package com.uwetrottmann.shopr.provider;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
 public class ShoprProvider extends ContentProvider {
 
+	private static UriMatcher sUriMatcher;
+
+	private static final int ITEMS = 100;
+	private static final int ITEM_ID = 101;
+
+	private static final int SHOPS = 200;
+	private static final int SHOP_ID = 201;
+
+	/**
+	 * Build and return a {@link UriMatcher} that catches all {@link Uri}
+	 * variations supported by this {@link ContentProvider}.
+	 */
+	private static UriMatcher buildUriMatcher(Context context) {
+		final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+		final String authority = ShoprContract.CONTENT_AUTHORITY;
+
+		// Items
+		matcher.addURI(authority, ShoprContract.PATH_ITEMS, ITEMS);
+		matcher.addURI(authority, ShoprContract.PATH_ITEMS + "/*", ITEM_ID);
+
+		// Shops
+		matcher.addURI(authority, ShoprContract.PATH_SHOPS, SHOPS);
+		matcher.addURI(authority, ShoprContract.PATH_SHOPS + "/*", SHOP_ID);
+
+		return matcher;
+	}
+
 	@Override
 	public boolean onCreate() {
-		// TODO Auto-generated method stub
-		return false;
+		final Context context = getContext();
+        sUriMatcher = buildUriMatcher(context);
+        return true;
 	}
 
 	@Override
