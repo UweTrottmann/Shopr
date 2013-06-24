4e0b203e70c187339966c1939fccccd518225492
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-24 11:27:32 +0200

    Database setup.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/provider/ShoprDatabase.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/provider/ShoprDatabase.java
@@ -2,15 +2,18 @@ package com.uwetrottmann.shopr.provider;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class ShoprDatabase extends SQLiteOpenHelper {
 
-	public ShoprDatabase(Context context, String name, CursorFactory factory,
-			int version) {
-		super(context, name, factory, version);
-		// TODO Auto-generated constructor stub
+	public static final String DATABASE_NAME = "shopr.db";
+	
+	public static final int DBVER_INITIAL = 1;
+
+	public static final int DATABASE_VERSION = DBVER_INITIAL;
+	
+	public ShoprDatabase(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
 	}
 
 	@Override
