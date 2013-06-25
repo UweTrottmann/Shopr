5f31b25e1e7255088a78b6a3c996c0d6ff20ca54
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-06-25 14:16:03 +0200

    Add max recommendations setting.

---
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/settings/AppSettings.java
@@ -0,0 +1,18 @@
+
+package com.uwetrottmann.shopr.settings;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Quick access to some app settings.
+ */
+public class AppSettings {
+
+    private static final String KEY_MAX_RECOMMENDATIONS = "com.uwetrottmann.shopr.maxrecommendations";
+
+    public static int getMaxRecommendations(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getInt(
+                KEY_MAX_RECOMMENDATIONS, 8);
+    }
+}
