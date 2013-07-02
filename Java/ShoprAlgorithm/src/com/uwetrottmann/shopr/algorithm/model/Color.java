5656278e9eb392d30adc2f2f2a33573e7917f5a5
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-07-02 18:05:38 +0200

    Add sample similar colors to Color.

--- Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/model/Color.java
+++ Java/ShoprAlgorithm/src/com/uwetrottmann/shopr/algorithm/model/Color.java
@@ -11,18 +11,18 @@ public class Color extends GenericAttribute {
 
     public enum Value implements AttributeValue {
         BLUE("Blue"),
+        RED("Red"),
+        PINK("Pink"), // rosa zu Deutsch
+        VIOLET("Violet"),
+        YELLOW("Yellow"),
         BROWN("Brown"),
         COLORED("Colored"),
-        YELLOW("Yellow"),
         MIXED("Mixed"),
         GREY("Grey"),
         GREEN("Green"),
         ORANGE("Orange"),
-        PINK("Pink"), // rosa zu Deutsch
-        RED("Red"),
         BLACK("Black"),
         TURQUOISE("Turquoise"),
-        VIOLET("Violet"),
         WHITE("White"),
         BEIGE("Beige");
 
@@ -119,5 +119,95 @@ public class Color extends GenericAttribute {
     @Override
     public AttributeValue[] getValueSymbols() {
         return Value.values();
+    }
+
+    @Override
+    public void likeValue(int valueIndex, double[] weights) {
+        Value[][] similarity = new Value[Value.values().length][];
+        similarity[Value.RED.index()] = new Value[] {
+                Value.PINK, Value.VIOLET
+        };
+        similarity[Value.BLUE.index()] = new Value[] {
+                Value.VIOLET, Value.TURQUOISE
+        };
+        similarity[Value.WHITE.index()] = new Value[] {
+                Value.GREY, Value.BEIGE
+        };
+        similarity[Value.BLACK.index()] = new Value[] {
+                Value.GREY, Value.BLUE, Value.BROWN
+        };
+
+        if (similarity[valueIndex] == null) {
+            super.likeValue(valueIndex, weights);
+        } else {
+            // increase by the average weight
+            double weightIncrease = 1.0 / weights.length;
+
+            /*
+             * If the value was 0.0 (disliked) increase double so it will have
+             * definitely have highest weight.
+             */
+            if (weights[valueIndex] == 0.0) {
+                weightIncrease *= 2;
+            }
+
+            weights[valueIndex] += weightIncrease;
+
+            // sum can not exceed 1.0
+            if (weights[valueIndex] > 1.0) {
+                Arrays.fill(weights, 0.0);
+                weights[valueIndex] = 1.0;
+                return;
+            }
+
+            // subtract average from other weights, BUT only from non-similar
+            // values
+            double redistributed = weightIncrease
+                    / (weights.length - 1 - similarity[valueIndex].length);
+            for (int i = 0; i < weights.length; i++) {
+                if (i != valueIndex && !isSimilarValue(similarity[valueIndex], i)) {
+                    weights[i] -= redistributed;
+                    // floor at 0.0
+                    if (weights[i] < 0) {
+                        weights[i] = 0.0;
+                    }
+                }
+            }
+
+            // sum can not exceed 1.0
+            double sum = 0;
+            for (int i = 0; i < weights.length; i++) {
+                sum += weights[i];
+            }
+            if (sum > 1.0) {
+                // distribute remaining weight over all similar values
+                double redistribute = 1 - weights[valueIndex];
+                for (int i = 0; i < weights.length; i++) {
+                    if (i != valueIndex && isSimilarValue(similarity[valueIndex], i)) {
+                        weights[i] = redistribute;
+                    } else {
+                        weights[i] = 0.0;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks whether one of the values has the given index.
+     */
+    private boolean isSimilarValue(Value[] values, int index) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].index() == index) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void dislikeValue(int valueIndex, double[] weights) {
+        // TODO Auto-generated method stub
+        super.dislikeValue(valueIndex, weights);
     }
 }
