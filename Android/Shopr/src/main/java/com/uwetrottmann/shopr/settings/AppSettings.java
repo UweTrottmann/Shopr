6c5f7600279c408632f85d4aa23edc2037f6af7b
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-07-02 15:48:18 +0200

    Fake location setting.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/settings/AppSettings.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/settings/AppSettings.java
@@ -10,9 +10,15 @@ import android.preference.PreferenceManager;
 public class AppSettings {
 
     private static final String KEY_MAX_RECOMMENDATIONS = "com.uwetrottmann.shopr.maxrecommendations";
+    private static final String KEY_FAKE_LOCATION = "com.uwetrottmann.shopr.fakelocation";
 
     public static int getMaxRecommendations(Context context) {
         return PreferenceManager.getDefaultSharedPreferences(context).getInt(
                 KEY_MAX_RECOMMENDATIONS, 9);
+    }
+
+    public static boolean isUsingFakeLocation(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(KEY_FAKE_LOCATION,
+                false);
     }
 }
