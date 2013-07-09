308b9b2e821c419465c256792b5afe6e6974e96a
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-07-09 12:13:04 +0200

    Store stats when finishing task.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/utils/Statistics.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/utils/Statistics.java
@@ -1,6 +1,12 @@
 
 package com.uwetrottmann.shopr.utils;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+
+import com.uwetrottmann.shopr.provider.ShoprContract.Stats;
+
 /**
  * Stores data about the current task.
  */
@@ -38,13 +44,28 @@ public class Statistics {
         mCycleCount++;
     }
 
-    public synchronized void stopTask() {
+    /**
+     * Stops the task and writes all data to the database.
+     * 
+     * @return The {@link Uri} pointing to the new data set.
+     */
+    public synchronized Uri finishTask(Context context) {
         if (!mIsStarted) {
-            return;
+            return null;
         }
+
         mIsStarted = false;
         long duration = System.currentTimeMillis() - mStartTime;
-        // TODO: write to database
+
+        // Write to database
+        ContentValues statValues = new ContentValues();
+        statValues.put(Stats.USERNAME, mUserName);
+        statValues.put(Stats.TASK_TYPE, mIsDiversity ? "div" : "sim");
+        statValues.put(Stats.CYCLE_COUNT, mCycleCount);
+        statValues.put(Stats.DURATION, duration);
+        final Uri inserted = context.getContentResolver().insert(Stats.CONTENT_URI, statValues);
+
+        return inserted;
     }
 
 }
