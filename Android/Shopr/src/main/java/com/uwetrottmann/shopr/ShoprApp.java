89bd0c029a2ef26f7d6176399364f5cf42d37b7c
Uwe Trottmann <uwe.trottmann@gmail.com>
2013-07-17 08:41:08 +0200

    Set global GA context.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/ShoprApp.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/ShoprApp.java
@@ -4,6 +4,8 @@ package com.uwetrottmann.shopr;
 import android.app.Application;
 import android.preference.PreferenceManager;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 public class ShoprApp extends Application {
 
     @Override
@@ -11,6 +13,8 @@ public class ShoprApp extends Application {
         super.onCreate();
 
         PreferenceManager.setDefaultValues(this, R.xml.prefs, false);
+
+        EasyTracker.getInstance().setContext(this);
     }
 
 }
